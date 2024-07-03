7877edbb27ad5c632b5f8c84e38fc905b5ce5eff
HanJuYoung309 <juyg407@naver.com>
2024-07-03 14:48:33 +0900

    feat/ì숙소 ì수정

--- src/main/java/com/project/mybnb/article/controller/ArticleController.java
+++ src/main/java/com/project/mybnb/article/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.project.mybnb.article.controller;
 
 import com.project.mybnb.article.domain.Article;
+import com.project.mybnb.article.dto.ArticlePatchDto;
 import com.project.mybnb.article.dto.ArticlePostDto;
 import com.project.mybnb.article.dto.ArticleResponseDto;
 import com.project.mybnb.article.mapper.ArticleMapper;
@@ -10,10 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,6 +32,16 @@ public class ArticleController {
 
       return  new ResponseEntity (articleMapper.createToResposnseDto(article),HttpStatus.CREATED);
 
+    }
+
+    //숙소 수정
+    @PatchMapping("/{article-id}")
+    public ResponseEntity updateArticle(@Valid @RequestBody ArticlePatchDto articleDto,
+    @PathVariable("article-id") long article_id) throws Exception {
+
+        Article article= articleService.updateArticle(articleMapper.updateArticleDto(articleDto),article_id);
+
+        return new ResponseEntity (articleMapper.updateToResposnseDto(article),HttpStatus.OK);
     }
 
 }
